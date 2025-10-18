846615ef782cae72be3beb8daa2f9ada6a451d79
Nano525 <20243ds020@utez.edu.mx>
2025-10-18 10:47:51 -0600

    Prueba

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        //Aqui se crea el objeto
+        Mouse miMouse = new Mouse("Negro", 3, "Usb", 20000);
+
+        // Y aqui se llaman a sus metodos
+        miMouse.hacerClic();
+        miMouse.hacerClic2();
+    }
 }
